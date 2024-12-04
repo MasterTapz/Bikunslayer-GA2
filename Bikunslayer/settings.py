fb40edc550c8290c00d5a684f5e0cec0ed33b6e2
skibidiyo <diyobanu@gmail.com>
2024-12-04 20:23:29 +0700

    bisa bikin user sm worker

--- Bikunslayer/settings.py
+++ Bikunslayer/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'Bikunslayer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'anindiyobanu',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',  # E.g., 'containers-us-west-123.railway.app'
+        'PORT': '5432',  # E.g., '5432'
     }
 }
+
 
 
 # Password validation
