1f5df08e94fbe52e116a17de4935c862735f9ada
Brian <briansubekti11@gmail.com>
2024-12-07 22:52:20 +0700

    MyOrder

--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import book_service, check_all_ids, check_database_connection, join_subcategory, leave_subcategory, my_orders,view_categories,view_subcategory_detail,view_subcategory_detail_worker
+from .views import book_service, cancel_order, check_all_ids, check_database_connection, edit_order, join_subcategory, leave_subcategory, my_orders,view_categories,view_subcategory_detail,view_subcategory_detail_worker
 
 urlpatterns = [
     path('check-db/', check_database_connection),
@@ -12,6 +12,8 @@ urlpatterns = [
     path('book-service/<uuid:subcategory_id>/<int:session>/', book_service, name='book_service'),
     path('subcategory/<uuid:subcategory_id>/worker/<uuid:worker_id>/join/', join_subcategory, name='join_subcategory'),
     path('my-orders/', my_orders, name='my_orders'),
+    path('edit-order/<uuid:order_id>/', edit_order, name='edit_order'),
+    path('cancel-order/<uuid:order_id>/', cancel_order, name='cancel_order'),
 
 
 
